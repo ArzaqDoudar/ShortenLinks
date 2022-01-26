57b693ef2ab4e1cc592f022cd0d289d2e0451ff6
Arzaq Ziad <arzaqziad20@gmail.com>
2022-01-27 00:20:26 +0200

    first commit

---
+++ database/migrations/2022_01_26_133642_create_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id(); // id BIGINT UNSIGNED AutoIncrement PrimaryKey 
+            // same as  $table->bigInteger('id')->unsigned()->autoIncrement()->primary(); 
+            
+            $table->string('link' , 1024);
+            $table->string('linkCode' , 50)->unique();  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
